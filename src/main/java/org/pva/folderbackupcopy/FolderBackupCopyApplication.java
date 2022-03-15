bff85d1d71ae7dc6e4c6616b8dbf4eab13fbc8d5
ViktorVx <victorptrv@yandex.ru>
2022-03-15 21:52:59 +0300

    Init commit

---
+++ src/main/java/org/pva/folderbackupcopy/FolderBackupCopyApplication.java
@@ -0,0 +1,13 @@
+package org.pva.folderbackupcopy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FolderBackupCopyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FolderBackupCopyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/pva/folderbackupcopy/FolderBackupCopyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pva.folderbackupcopy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FolderBackupCopyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
